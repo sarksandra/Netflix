7d803e405508bccd15af246e67fdc00ada1ad884
opilane <opilane@HP-CZC0359NP0>
2025-05-12 13:09:43 +0300

    Add project files.

---
+++ Netflix/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Netflix
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Netflix/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+
+namespace Netflix;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Poppins-Regular.ttf", "PoppinsRegular");
+				fonts.AddFont("Poppins-Semibold.ttf", "PoppinsSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
